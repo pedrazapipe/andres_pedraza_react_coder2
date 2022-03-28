6b6c8c64109b670dab40f6bf6c9cc3ef66851672
Andrés Pedraza <pedrazapipe@gmail.com>
2022-03-28 01:06:33 -0500

    Desafio 2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './scss/app.scss';
+import Navbar from "./componets/Navbar";
+
+function App() {
+
+  return (
+
+    <Navbar></Navbar>
+    
+  );
+}
+
+export default App;
---
+++ src/componets/Navbar.js
@@ -0,0 +1,32 @@
+/* Inicio Header pagina */
+
+const Navbar = () => {
+
+    return (
+
+        <header>
+    
+            <h1 className="header__titulo">FunkoPops<a href=""></a></h1>
+
+                <nav className="header__navegacion">
+
+                    <ul>
+
+                        <li><a href="">Redes sociales</a></li>              
+                        <li><a href="">Productos</a></li>  
+                        <li><a href="">Contacto</a></li>  
+        
+                    </ul>
+
+                </nav>
+
+        
+        </header>
+
+    );
+
+};
+
+export default Navbar;
+
+/* Final Header pagina */
